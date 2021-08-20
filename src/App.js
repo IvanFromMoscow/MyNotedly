63d2921724f4a74c70a7ecf34905ad83751f3b99
IvanFromMoscow <ibusygin1988@gmail.com>
2021-08-20 23:45:18 +0300

    Глава 14 - GraphQL клиент взаимодействие через ApolloClient

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React  from 'react';
 import ReactDOM  from 'react-dom';
-
+import { ApolloClient, ApolloProvider, InMemoryCache, AppoloProvider } from '@apollo/client';
 import GlobalStyle from '/components/GlobalStyle';
 
+
 import Pages from './pages';
+
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+    uri,
+    cache,
+    connectToDevTools: true
+});
 const App = () => {
     return (
-        <div>
-            <GlobalStyle/>
-           <Pages />
-        </div>
+      <ApolloProvider client={client}>
+            <div>
+                <GlobalStyle/>
+               <Pages />
+            </div>
+      </ApolloProvider>
     )
 };
 
