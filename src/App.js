0de19e578fa85db5e6f01eebb66583272082c5c3
IvanFromMoscow <ibusygin1988@gmail.com>
2021-08-25 22:39:35 +0300

    Глава 16 - добавлен CRUD для заметок

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React  from 'react';
 import ReactDOM  from 'react-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache, AppoloProvider } from '@apollo/client';
+import {    ApolloClient, 
+            ApolloProvider, 
+            InMemoryCache, 
+            createHttpLink
+        } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 import GlobalStyle from '/components/GlobalStyle';
 
 
 import Pages from './pages';
 
 const uri = process.env.API_URI;
+const httpLink = createHttpLink({ uri });
 const cache = new InMemoryCache();
 
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            authorization: localStorage.getItem('token') || ''
+        }
+    }
+})
 const client = new ApolloClient({
-    uri,
+    link: authLink.concat(httpLink),
     cache,
+    resolvers: {
+    },
     connectToDevTools: true
 });
+
+// check for a local token
+const data = {
+    isLoggedIn: !!localStorage.getItem('token')
+  };
+cache.writeData({ data });
+
+client.onResetStore(() => cache.writeData({ data }));
 const App = () => {
     return (
       <ApolloProvider client={client}>
